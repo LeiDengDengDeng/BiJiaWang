dcd124e0c12b786c4e0824d2ef079c956d82ae4e
罗雪纯 <luoxuechun@luoxuechundeMBP.lan>
2017-04-13 19:57:19 +0800

    同步一些接口
    11

---
+++ src/main/java/com/repository/SynonymsRepository.java
@@ -0,0 +1,10 @@
+package com.repository;
+
+import com.model.SynonymsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by luoxuechun on 2017/4/13.
+ */
+public interface SynonymsRepository extends JpaRepository<SynonymsEntity,Integer>{
+}
