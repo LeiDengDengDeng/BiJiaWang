124e7e283cffe3ee29ff478ff3d912fa9b1fc78d
liying <470549092@qq.com>
2017-04-13 20:51:40 +0800

    小修改

--- src/main/java/com/service/ShieldServiceImpl.java
+++ src/main/java/com/service/ShieldServiceImpl.java
@@ -1,10 +1,14 @@
 package com.service;
 
 import com.model.GoodEntity;
+import com.model.GoodshieldEntity;
+import com.repository.GoodRepository;
+import com.repository.GoodsheildRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,10 +18,19 @@ import java.util.List;
 @Transactional
 public class ShieldServiceImpl implements ShieldService {
 
+    @Autowired
+    private GoodsheildRepository goodsheildRepository;
+
+    @Autowired
+    private GoodRepository goodRepository;
     @Override
     public List<GoodEntity> shield(List<GoodEntity> goodsEntity) {
-        //todo 获取屏蔽商品列表
-        List<GoodEntity> shieldList=null;
+        //获取屏蔽商品列表
+        List<GoodEntity> shieldList=new ArrayList<>();
+        List<GoodshieldEntity> shieldId=goodsheildRepository.findAll();
+        for (GoodshieldEntity gse:shieldId){
+            shieldList.add(goodRepository.findOne(gse.getGoodId()));
+        }
 
         //删除负面商品
         for(GoodEntity goods:goodsEntity){
