412dc42e90ba15c23b43c3503cf4698c38858cf7
罗雪纯 <luoxuechun@luoxuechundeMBP.lan>
2017-04-13 21:27:40 +0800

    蒋香香
    11

--- src/main/java/com/repository/CommentRepository.java
+++ src/main/java/com/repository/CommentRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by luoxuechun on 2017/4/13.
  */
@@ -12,6 +14,6 @@ import org.springframework.stereotype.Repository;
 public interface CommentRepository extends JpaRepository<CommentEntity,Integer> {
 
   @Query("select c.content from CommentEntity c where c.goodByGoodId.goodId=?1")
-    CommentEntity getComment(int gooid);
+  List<CommentEntity> getComment(int gooid);
 
 }
