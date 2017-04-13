0e071e71a5694cd6ac9db9ad8b89698c6e5fdcae
liying <470549092@qq.com>
2017-04-13 19:39:31 +0800

    小修改

--- src/main/java/com/service/SearchServiceImpl.java
+++ src/main/java/com/service/SearchServiceImpl.java
@@ -1,9 +1,25 @@
 package com.service;
 
 import com.model.GoodEntity;
+import com.repository.GoodRepository;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleFragmenter;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,9 +28,22 @@ import java.util.List;
 @Service("searchService")
 @Transactional
 public class SearchServiceImpl implements SearchService{
+
+    @Autowired
+    private GoodRepository goodRepository;
+
+
     @Override
     public List<GoodEntity> analyse(String[] keywords) {
+        //匹配关键词
+        for (String key:keywords){
 
-        return null;
+
+
+        }
+
+        List<GoodEntity> goodEntities=goodRepository.findAll();
+            return goodEntities;
+
     }
 }
